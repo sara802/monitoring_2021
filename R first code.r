7d02e79126da580f401b557d06c1410be923df4c
sara802 <62662082+sara802@users.noreply.github.com>
2020-11-02 10:59:50 +0100

    Update R first code.r

--- R first code.r
+++ R first code.r
@@ -1,1 +1,26 @@
 
+
+#this is an array
+primates<-c(3,5,9,15,40)
+primates
+
+#new set
+crabs <- c(100,70,30,10,5)
+crabs
+
+#first plot in R
+plot(primates,crabs,col="red")
+
+plot(primates,crabs,col="red",pch=19)
+
+plot(primates,crabs,col="red",pch=19, cex=2)
+plot(primates,crabs,col="red",pch=19, cex=6)
+
+plot(primates,crabs,col="red",pch=19, cex=6,main="my first ecological graph in R!")
+
+#create 
+ecoset <- data.frame(primates,crabs)
+ecoset
+#(3+5+9+15+40)/5
+#summary of the dataframe
+
