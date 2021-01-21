60eda2502829cd67990d847977da254227752e73
sara802 <62662082+sara802@users.noreply.github.com>
2021-01-21 18:20:37 +0100

    Update R_code_energy.r

--- R_code_energy.r
+++ R_code_energy.r
@@ -34,3 +34,76 @@ plot(copNDVI, col=clymax)
 ext <- c(0,20,35,55)  # xmin xmax ymin ymax
 copNDVI_Italy <- crop(copNDVI, ext)
 plot(copNDVI_Italy, col=clymax)
+
+
+
+# R_code_energy.r
+
+############# DAY 2
+# Deforestation example
+library(raster)
+library(RStoolbox)
+
+
+#set wd
+setwd("C:/lab/") 
+
+defor1 <- brick("defor1_.jpg")  
+
+plotRGB(defor1, r=1, g=2, b=3, stretch="Lin")
+
+# Exercise: import the defor2_.jpg image
+defor2 <- brick("defor2_.jpg")  
+
+plotRGB(defor2, r=1, g=2, b=3, stretch="Lin")
+
+# Exercise: put images one beside the other with par()
+par(mfrow=c(2,1))
+plotRGB(defor1, r=1, g=2, b=3, stretch="Lin")
+plotRGB(defor2, r=1, g=2, b=3, stretch="Lin")
+
+
+# dvi for the first period
+dvi1 <- defor1$defor1_.1 - defor1$defor1_.2
+
+# dev.off()
+plot(dvi1)
+
+cl <- colorRampPalette(c('darkblue','yellow','red','black'))(100) # specifying a color scheme
+plot(dvi1, col=cl)
+
+# dvi2 after the cut!
+dvi2 <- defor2$defor2_.1 - defor2$defor2_.2
+cl <- colorRampPalette(c('darkblue','yellow','red','black'))(100) # specifying a color scheme
+plot(dvi2, col=cl)
+
+# Exercise: put images one beside the other with par()
+par(mfrow=c(2,1))
+plot(dvi1, col=cl, main="DVI before cut")
+plot(dvi2, col=cl, main="DVI after cut")
+
+# difference biomass before cut and after cut
+difdvi <- dvi1 -dvi2
+# dev.off()
+plot(difdvi)
+
+cldif <- colorRampPalette(c('blue','white','red'))(100) 
+plot(difdvi, col=cldif, main="amount of energy lost!")
+
+hist(difdvi, col="red")
+
+####### final par!
+# defor1
+# defor2
+# dvi1
+# dvi2
+# difdvi
+# histogram
+
+par(mfrow=c(3,2))
+plotRGB(defor1, r=1, g=2, b=3, stretch="Lin")
+plotRGB(defor2, r=1, g=2, b=3, stretch="Lin")
+plot(dvi1, col=cl, main="biomass before cut")
+plot(dvi2, col=cl, main="biomass after cut")
+plot(difdvi, col=cldif, main="amount of energy lost!")
+hist(difdvi, col="red")
