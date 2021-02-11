a112eb21d8bd3cd71a12aedd5eddbd30bf975a4f
sara802 <62662082+sara802@users.noreply.github.com>
2021-02-11 17:06:26 +0100

    Update R first code.r

--- R first code.r
+++ R first code.r
@@ -9,20 +9,31 @@ crabs <- c(100,70,30,10,5)
 crabs
 
 #first plot in R
+
+plot(primates,crabs)
+
+#plot with color...col=DOUBLE QUOTES
 plot(primates,crabs,col="red")
 
+#fill in circles by adding pch to list in brackets
 plot(primates,crabs,col="red",pch=19)
 
+#change size of symbol(circle)with cex
 plot(primates,crabs,col="red",pch=19, cex=2)
 plot(primates,crabs,col="red",pch=19, cex=6)
 
+#main added as to make a title name,main=DOUBLE QUOTES
 plot(primates,crabs,col="red",pch=19, cex=6,main="my first ecological graph in R!")
-#main added as to make a title name 
-
-#create 
+ 
+#CREATE A DATAFRAME....use function data.frame assign to ecoset
 ecoset <- data.frame(primates,crabs)
+
 ecoset
-#(3+5+9+15+40)/5
-#summary of the dataframe
+
+#find mean value per site
+(3+5+9+15+40)/5
+(100+70+30+10+5)/5
+
+#summary of the dataframe...we can see min,1st 2nd 3rd quantile,max,median and mean....
 summary(ecoset)
 
