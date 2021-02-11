1e61b5c6fa70d9cad43162b328ca9338e5a59b10
sara802 <62662082+sara802@users.noreply.github.com>
2021-02-11 17:42:33 +0100

    Update R_code_plot.r

--- R_code_plot.r
+++ R_code_plot.r
@@ -1,13 +1,18 @@
 #Lets plot the relationship among ecological variables
+#Classes and methods for spatial data; the classes document where the spatial location information resides, for 2D or 3D data....
 
+#THE RELATIONSHIP AMONG VARIABLES
+#WE WILL USE SP PACKAGE AND SP LIBRARY
 
 #install.packages is installed 
 install.packages("sp")
 
+#CALL THE LIBRARY
 library(sp)
 #no use of "because sp in already now in R
+
 data(meuse)
-#look inside the chat!
+#look inside the SET!
 meuse
 
 
@@ -19,11 +24,12 @@ head(meuse)
 
 #mean of all of the variables
 #cadmium crazy mean extraction
-#(11,7+8,6+6,5,...)/N
+(11,7+8,6+6,5,...)/N
+
 summary(meuse)
 
 #exercise plot zinc (y) against cadmium (x)
-#$
+#$ DOLLAR SIGN to connect them
 plot(meuse$cadmium, meuse$zinc)
  #use attach function unstead of dollar
 
@@ -31,17 +37,15 @@ attach(meuse)
  plot(cadmium, zinc)
 plot(cadmium, lead)
 
-#trivial or
-#you can use scatterplot matrices
+#trivial or you can use SCATTER PLOT matrixes
+#A scatter plot matrix is a grid (or matrix) of scatter plots used to visualize bivariate relationships between combinations of variables
+#Each scatter plot in the matrix visualizes the relationship between a PAIR of variables, allowing many relationships to be explored in one chart!
 pairs(meuse)
-
-
-
-
 
 #second part
 #recall sp package ,dataset meuse
 library(sp)
+
 data(meuse)
 
 pairs(meuse)
@@ -57,7 +61,7 @@ pairs(meuse[,3:6])
 
 # let's use the names of the columns:
 pairs(~ cadmium + copper + lead + zinc, data=meuse) 
-# tilde is going to be done by AltGr + ^
+# tilde is going to be done by AltGr + ^ 
 
 # let's prettify the graph
 # Exercise: just use cadmium, lead and zinc
@@ -68,16 +72,18 @@ pairs(~ cadmium + lead + zinc, data=meuse)
 pairs(~ cadmium + copper + lead + zinc, data=meuse, col="red") 
 
 # for the future change colors of single panels by the par() function
-#par()COLOR CHANGE
+#par()
+# R programming has a lot of graphical parameters which control the way our graphs are displayed.
+#The par() function helps us in setting or inquiring about these parameters...
 
+#COLOR CHANGE
 # Exercise: change the symbol to filled triangles: pch
 #pch= FILL TRIANGLE
-
 pairs(~ cadmium + copper + lead + zinc, data=meuse, col="red", pch=17) 
-
 
 # Exercise: increase the size of the triangles
 #cex= SIZE TRIANGLE
+
 pairs(~ cadmium + copper + lead + zinc, data=meuse, col="red", pch=17, cex=3)
 
 
